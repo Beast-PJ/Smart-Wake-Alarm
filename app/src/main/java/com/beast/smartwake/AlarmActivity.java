220d2da8aa76a4e76c78b8c5848e2752748b6e90
Beast PJ <p.jadhav61657@gmail.com>
2024-07-27 02:04:28 +0530

    Basic Elements Defined

--- app/src/main/java/com/beast/smartwake/AlarmActivity.java
+++ app/src/main/java/com/beast/smartwake/AlarmActivity.java
@@ -3,7 +3,7 @@ package com.beast.smartwake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Spinner;
+
 
 public class AlarmActivity extends AppCompatActivity {
 
